0523ebfaae81e8a7802dbca8f41d014c2be915f1
Chris W. Gerber <chriswgerber@gmail.com>
2015-05-12 12:03:46 -0500

    Updated Build Script.

--- build/wordpress_build.sh
+++ build/wordpress_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BUILD=`pwd`;
+PLUGIN_DIR='dfp-ads';
 
 echo 'Building WordPress plugin.';
 
@@ -11,28 +12,34 @@ echo "$VERSION";
 cd $BUILD/../;
 
 ###
-# Change Version Number
-###
-echo "Updating version to $VERSION";
-sed -i '' "s|\* Version:           *.*.*|\* Version:           $VERSION|" plugin.php
-
-###
 # Make directory of files to import
 ###
-mkdir plugin/;
-rsync -av --files-from=build/wordpress-files.txt . plugin/
+mkdir "$PLUGIN_DIR/";
+rsync -av --files-from=build/wordpress-files.txt . "$PLUGIN_DIR/"
 
 ###
 # Move vendor files to lib folder
 ###
 mkdir lib;
-rsync -vah vendor/ plugin/lib/
+rsync -vah vendor/ "$PLUGIN_DIR/lib"
+
+###
+# Change Version Number
+###
+echo "Updating version to $VERSION";
+sed -i '' "s|\* Version:           *.*.*|\* Version:           $VERSION|" "$PLUGIN_DIR/plugin.php"
 
 ###
 # Change autoload
 ###
 echo "Changing location of autoload";
-sed -i '' "s|require_once 'vendor/autoload.php'|require_once 'lib/autoload.php'|" plugin/plugin.php
+sed -i '' "s|require_once 'vendor/autoload.php'|require_once 'lib/autoload.php'|" "$PLUGIN_DIR/plugin.php"
+
+###
+# Zip plugin for distribution
+###
+zip -r -X dfp-ads.zip "$PLUGIN_DIR/"
+rm -rf "$PLUGIN_DIR/"
 
 ###
 # Return Home; We're Complete
