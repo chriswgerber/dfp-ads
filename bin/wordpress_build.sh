a508cb8875901d4ea487cc25f7994910fdba9271
Chris W. Gerber <chriswgerber@gmail.com>
2015-07-16 13:22:43 -0500

    New build.

--- bin/wordpress_build.sh
+++ bin/wordpress_build.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
 BUILD=`pwd`;
-REPO_DIR='repo/trunk';
+REPO_DIR='repo';
+
+### Getting back to the plugin root
+cd_plugin_dir
+{
+    cd "$BUILD/../";
+}
 
 echo 'Building WordPress plugin.';
 echo -n 'What is the version number? ';
 read VERSION;
 echo "$VERSION";
 
-cd $BUILD/../;
+### Revert vendor files to remove dev
+cd_plugin_dir
+echo "Updating Composer packages to release packages.";
+composer update --no-dev
 
-###
-# Move vendor files to lib folder
-###
-rsync -vah --exclude ".*/" vendor/ "$REPO_DIR/vendor"
+### Make directory of files to import
+rsync -hav --exclude ".*/" --files-from=bin/wordpress-files.txt . "$REPO_DIR/trunk"
 
-###
-# Make directory of files to import
-###
-rsync -hav --exclude ".*/" --files-from=bin/wordpress-files.txt . "$REPO_DIR/"
-
-###
-# Change Version Number
-###
+### Change Version Number
 echo "Updating version to $VERSION";
-sed -i '' "s|\* Version:           *.*.*|\* Version:           $VERSION|" "$REPO_DIR/plugin.php"
+sed -i '' "s|\* Version:           *.*.*|\* Version:           $VERSION|" "$REPO_DIR/trunk/plugin.php"
+sed -i '' "s|Stable tag: *.*.*|Stable tag: $VERSION|" "$REPO_DIR/trunk/readme.txt"
 
-###
-# Updating Repo
-###
-cd repo;
+### Updating Repo
+cd $REPO_DIR;
 svn stat
 echo -n "Is it okay to commit this to the WordPress repo? (y/n) "
 read CONFIRM
@@ -39,9 +38,7 @@ if [ "$CONFIRM" == 'y' ]; then
     svn ci -m "$MESSAGE";
 fi
 
-###
-# Add New Release
-###
+### Add New Release
 echo -n "Is this a release version? (y/n) "
 read CONFIRM_VER
 if [ "$CONFIRM_VER" == 'y' ]; then
@@ -49,8 +46,14 @@ if [ "$CONFIRM_VER" == 'y' ]; then
     svn ci -m "Version $VERSION";
 fi
 
-###
-# Return Home; We're Complete
-###
+### Documentation
+cd_plugin_dir
+php vendor/bin/phpdoc run -f ./plugin.php -d ./includes -t build/docs/
+
+### Revert vendor files to remove dev
+echo "Reverting composer packages back to development.";
+composer update
+
+### Return Home; We're Complete
 cd $BUILD;
 echo 'Build Complete.';
